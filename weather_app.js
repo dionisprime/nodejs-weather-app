fe664e1766e6eb978963da9b05f38ad4aaf68b39
Dionis Prime <dioniskruspe@gmail.com>
2023-09-26 23:04:05 +0300

    переписал с промисов на async/await

--- weather_app.js
+++ weather_app.js
@@ -1,39 +1,42 @@
 const { format } = require('date-fns');
-const http = require('http'); // встроенный модуль http
-const server = http.createServer().listen(3000, () => {
-    console.log(`Server is listening...`);
-}); // создание сервера, порт 3000
+const http = require('http');
+const server = http.createServer().listen(3001, () => {
+    console.log('Server is listening...');
+});
 
 const HOST = 'http://localhost';
 const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
 const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-server.on('request', (req, res) => {
-    if (req.url === '/favicon.ico') {
-        return res.end();
-    }
-    const urlParams = new URL(HOST + req.url); // формируем объект URL с помощью встроенного класса
-    const cityName = urlParams.searchParams.get('city');
-    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
 
-    fetch(url)
-        .then((response) => {
-            if (!response.ok) {
-                res.end('Error');
-                throw new Error('Нет ответа');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            const citySunset = format(
-                new Date(data.sys.sunset * 1000),
-                'HH:mm'
-            );
-            const cityWeather = `conditions for ${data.name} is ${data.weather[0].main}. Sunset: ${citySunset}`;
-            console.log(cityWeather);
-            res.end(cityWeather);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.end('Ошибка!');
-        });
+server.on('request', async (req, res) => {
+    try {
+        if (req.url === '/favicon.ico') {
+            return res.end();
+        }
+        const urlParams = new URL(HOST + req.url);
+        const cityName = urlParams.searchParams.get('city');
+
+        const cityWeather = await getCityWeather(cityName);
+
+        console.log('cityWeather: ', cityWeather);
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end(cityWeather);
+    } catch (error) {
+        console.error('Error: ', error.message);
+        res.end(error.message);
+    }
 });
+
+async function getCityWeather(cityName) {
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+    const response = await fetch(url);
+    const weatherData = await response.json();
+
+    if (weatherData.cod === '404') {
+        throw new Error(weatherData.message);
+    }
+
+    const citySunset = format(new Date(weatherData.sys.sunset * 1000), 'HH:mm');
+
+    return `Conditions for ${weatherData.name} is ${weatherData.weather[0].main}. Sunset: ${citySunset}`;
+}
